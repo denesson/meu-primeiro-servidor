9e189a06c93de906243f0795e864028f0c7490f9
Denesson barreto <denessonbarretodeoliveira@hotmail.com>
2026-07-29 20:43:55 -0300

    Adicionado a rota de PUT e DELETE funcionando corretamente.

--- servidor.js
+++ servidor.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+let eventos = [
+  { id: 1, nome: "Festa na praia" },
+  { id: 2, nome: "Show de rock" }
+]
 
 app.get('/', (req, res) =>{
     res.send('Meu servidor está rodando')
@@ -14,6 +18,35 @@ app.get('/usuarios/:nome', (req, res) =>{
 app.post('/login', (req, res) =>{
     const { email, senha } = req.body
     res.send(`Recebi o login de: ${email}`)
+})
+
+app.get('/eventos', (req, res)=>{
+    res.json(eventos)
+})
+
+app.post('/eventos', (req, res) =>{
+    const { nome } = req.body
+    const novoEvento = {id: eventos.length + 1, nome: nome}
+    eventos.push(novoEvento)
+    res.json(eventos)
+})
+
+app.put('/eventos/:id', (req, res) =>{
+    const id = Number(req.params.id)
+    const { nome } = req.body
+    for(let i = 0; i < eventos.length; i++){
+        if(eventos[i].id === id){
+            eventos[i].nome = nome
+        }
+    }
+    res.json(eventos)
+})
+
+app.delete('/eventos/:id', (req, res) =>{
+    const id = Number(req.params.id)
+    eventos = eventos.filter( eventos => eventos.id !== id)
+
+    res.json(eventos)
 })
 
 app.listen(3000, () =>{
