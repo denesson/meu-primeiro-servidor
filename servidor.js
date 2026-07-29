5142d826d92b497a410fe9eaf0f3efad6a8642fd
Denesson barreto <denessonbarretodeoliveira@hotmail.com>
2026-07-29 18:55:22 -0300

    Adiciona rotas GET com parametro e POST de login

--- servidor.js
+++ servidor.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) =>{
     res.send('Meu servidor está rodando')
+})
+
+app.get('/usuarios/:nome', (req, res) =>{
+    const nome = req.params.nome
+    res.send(`Vc buscou pelo usuário: ${nome}`)
+})
+
+app.post('/login', (req, res) =>{
+    const { email, senha } = req.body
+    res.send(`Recebi o login de: ${email}`)
 })
 
 app.listen(3000, () =>{
